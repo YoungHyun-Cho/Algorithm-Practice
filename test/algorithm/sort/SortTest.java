8e12ffc9b52617c519c7a64ab466f7b744faef33
YoungHyun-Cho <psyhyun1030@gmail.com>
2024-02-14 16:10:12 +0900

    test logic update

--- test/algorithm/sort/SortTest.java
+++ test/algorithm/sort/SortTest.java
@@ -1,24 +1,52 @@
 package algorithm.sort;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SortTest {
 
-    public static int[] arr = new int[] {
-            3, 11, 0, 13, 1, 2, 9, 17, 6, 16,
-            5, 19, 8, 4, 18, 12, 14, 7, 10, 15
-    };
-    public static SortAlgorithm sortAlgorithm = new BubbleSort();
+    private static int[] arr;
+    private static SortAlgorithm sortAlgorithm = new BubbleSort();
+    private static int testArraySize = 10000;
+
+    @BeforeEach
+    public void initiateArray() {
+
+        // 0 ~ testArraySize 까지의 배열 생성
+        int[] shuffled = new int[testArraySize];
+        for (int i = 0; i < shuffled.length; i++) {
+            shuffled[i] = i;
+        }
+
+        // 순서 섞기
+        Random random = new Random();
+        for (int i = shuffled.length - 1; i > 0; i--) {
+            int randomIndex = random.nextInt(i + 1);
+
+            // swap
+            int temp = shuffled[randomIndex];
+            shuffled[randomIndex] = shuffled[i];
+            shuffled[i] = temp;
+        }
+
+        arr = shuffled;
+    }
 
     @Test
     public void sortTest() {
 
+        long start = System.nanoTime();
         sortAlgorithm.sort(arr);
+        long end = System.nanoTime();
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < testArraySize; i++) {
             assertEquals(arr[i], i);
         }
+
+        System.out.println("소요 시간 : " + (end - start));
     }
 }
