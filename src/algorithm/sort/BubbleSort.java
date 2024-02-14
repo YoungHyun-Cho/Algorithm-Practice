85f75db92993de4771f117e0068333dfe1500f35
YoungHyun-Cho <psyhyun1030@gmail.com>
2024-02-14 18:29:22 +0900

    comment added

--- src/algorithm/sort/BubbleSort.java
+++ src/algorithm/sort/BubbleSort.java
@@ -2,6 +2,18 @@ package algorithm.sort;
 
 import java.util.Arrays;
 
+/*
+* # 버블 정렬
+* - 인접한 요소(좌, 우)들을 비교하고, 우측 요소보다 좌측 요소가 더 크다면, 양 요소의 자리를 바꿈(swap).
+* - 외부반복문이 1회 순회할 때마다,
+*   - 가장 큰 요소가 우측으로 밀림.
+*   - 따라서, 내부반복문의 다음 순회에서는 직전 순회 종료 시점 이전까지만 반복 비교 작업을 진행
+*
+* - 맨 앞에서부터 시작할 수도 있고, 맨 뒤에서부터 시작할 수도 있음.
+*   - 맨 앞에서부터 시작 : 외부반복문 매 순회마다 큰 요소들이 우측으로 모임.
+*   - 맨 뒤에서부터 시작 : 외부반복문 매 순회마다 작은 요소들이 좌측으로 모임.
+* */
+
 public class BubbleSort implements SortAlgorithm {
 
     @Override
@@ -14,9 +26,9 @@ public class BubbleSort implements SortAlgorithm {
     // 앞에서부터 버블정렬
     private void startFromFront(int[] arr) {
 
-        for (int i = 0; i < arr.length; i++) { // i : 중첩 반복문에서 각 반복 시 어디까지 순회해야 하는지에 대한 정보 제공
-            for (int j = 1; j < arr.length - i; j++) {
-                if (arr[j - 1] > arr[j]) swap(arr, j - 1, j);
+        for (int i = 0; i < arr.length; i++) {                    // i : 중첩 반복문에서 각 반복 시 어디까지 순회해야 하는지에 대한 정보 제공 (length - i까지 증가)
+            for (int j = 1; j < arr.length - i; j++) {            // 맨 앞에서부터 length - 1까지 두 요소를 비교하기 위해 순회
+                if (arr[j - 1] > arr[j]) swap(arr, j - 1, j); // j와 j 직전 요소를 비교하고, 직전 요소가 더 크다면 서로 자리를 바꿈.
             }
             System.out.println(Arrays.toString(arr));
         }
@@ -25,9 +37,9 @@ public class BubbleSort implements SortAlgorithm {
     // 뒤에서부터 버블정렬
     private void startFromBack(int[] arr) {
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = arr.length - 2; j >= i; j--) {
-                if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+        for (int i = 0; i < arr.length; i++) {                     // i : 중첩 반복문에서 각 반복 시 어디까지 순회해야 하는지에 대한 정보 제공 (i까지 감소)
+            for (int j = arr.length - 2; j >= i; j--) {            // 맨 뒤에서부터 i까지 두 요소를 비교하기 위해 순회
+                if (arr[j] > arr[j + 1]) swap(arr, j, j + 1); // j와 j 직후 요소를 비교하고, 직후 요소가 더 크다면 서로 자리를 바꿈.
             }
             System.out.println(Arrays.toString(arr));
         }
--- src/algorithm/sort/SelectionSort.java
+++ src/algorithm/sort/SelectionSort.java
@@ -2,6 +2,12 @@ package algorithm.sort;
 
 import java.util.Arrays;
 
+/*
+* # 선택 정렬
+* - 외부 반복문으로 자리를 지정하고, 해당 자리 이후에 존재하는 최소값을 지정한 자리의 값과 바꿈(swap)
+* - 내부 반복문으로 해당 자리 이후의 최소값을 탐색해야 함.
+* */
+
 public class SelectionSort implements SortAlgorithm {
 
     @Override
@@ -9,12 +15,12 @@ public class SelectionSort implements SortAlgorithm {
 
         System.out.println(Arrays.toString(arr));
 
-        for (int i = 0; i < arr.length; i++) {
-            int minIndex = i;
-            for (int j = i; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) minIndex = j;
+        for (int i = 0; i < arr.length; i++) {            // i : 최소값이 곧 들어올 자리 지정
+            int minIndex = i;                             // minIndex : 최소값이 존재하는 자리
+            for (int j = i + 1; j < arr.length; j++) {    // 최소값을 찾기 위해 i 바로 다음부터 배열 순회
+                if (arr[j] < arr[minIndex]) minIndex = j; // 최소값을 찾아 minIndex에 위치 저장
             }
-            swap(arr, i, minIndex);
+            swap(arr, i, minIndex);                       // 처음에 지정한 자리(i)와 최소값이 존재하는 위치의 값을 서로 바꿈
             System.out.println(Arrays.toString(arr));
         }
     }
