ed875cc0cef7ca1011bfa1fa49c03fdcf6e15815
YoungHyun-Cho <psyhyun1030@gmail.com>
2024-02-15 17:49:28 +0900

    shell sort

--- test/algorithm/sort/SortTest.java
+++ test/algorithm/sort/SortTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SortTest {
 
     private static int[] arr;
-    private static SortAlgorithm sortAlgorithm = new SelectionSort();
+    private static SortAlgorithm sortAlgorithm = new ShellSort();
     private static int testArraySize = 20;
 
     @BeforeEach
